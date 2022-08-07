1b3f95e4c4ba53bbfbf38e0b8dfd586e03dbfa07
Olly Seber <olly_s@hotmail.co.uk>
2022-08-06 20:02:28 -0700

    Fix bugs in Board class
    Performed an initial round of testing of the board class and fixed
    errors with all the methods.

--- script.rb
+++ script.rb
@@ -12,9 +12,6 @@
 #   Print updated board
 # END loop
 
-# Board class
-# Method: convert to string
-
 # Game class
 # Contains program flow data for interacting with board object
 
@@ -31,8 +28,8 @@ class Board
   ].freeze
 
   def initialize
-    @cells = Array.new(9, "")
-    @available_moves = [1..9]
+    @cells = Array.new(9, " ")
+    @available_moves = Array.new(9) { |i| i + 1 }
     @next_symbol = "X"
   end
 
@@ -41,10 +38,10 @@ class Board
   end
 
   def reset
-    @cells.each { |cell| cell = "" }
+    @cells.each_index { |i| @cells[i] = " " }
 
     @available_moves.clear
-    @available_moves = [1..9]
+    @available_moves = Array.new(9) { |i| i + 1 }
 
     @next_symbol = "X"
   end
@@ -52,25 +49,27 @@ class Board
   def move(position)
     return unless @available_moves.include?(position)
 
-    @cells[position] = @next_symbol
+    @cells[position - 1] = @next_symbol
     @available_moves.delete(position)
     toggle_next_symbol
   end
 
   def check_for_win
     WINNING_SEQUENCES.each do |sequence|
-      if (@cells[sequence[0]] == @cells[sequence[1]] &&
-        @cells[sequence[0]] == @cells[sequence[2]])
+      if (@cells[sequence[0]] == @cells[sequence[1]]) &&
+         (@cells[sequence[0]] == @cells[sequence[2]]) &&
+         (@cells[sequence[0]] != " ")
         return true
       end
-      false
+    end
+    false
   end
 
   def to_s
-    "#{@cells[0]} | #{@cells[1]} | #{@cells[2]}
-    -------
-    #{@cells[3]} | #{@cells[4]} | #{@cells[5]}
-    -------
-    #{@cells[7]} | #{@cells[8]} | #{@cells[9]}"
+    "#{@cells[0]} | #{@cells[1]} | #{@cells[2]}\n" \
+    "---------\n" \
+    "#{@cells[3]} | #{@cells[4]} | #{@cells[5]}\n" \
+    "---------\n" \
+    "#{@cells[6]} | #{@cells[7]} | #{@cells[8]}"
   end
 end
