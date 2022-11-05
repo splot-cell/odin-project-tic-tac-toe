d6b2b5de18fad029e1ae3189cd39496f78e16bc1
Olly Seber <olly_s@hotmail.co.uk>
2022-11-05 08:41:56 -0700

    Add test for Board#check_for_winner

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -3,6 +3,13 @@
 require_relative "../lib/tic_tac_toe"
 
 describe Board do
+  subject(:init_board) { described_class.new }
 
-
+  describe "#check_for_winner" do
+    context "When the Board is initialized" do
+      it "Returns nil" do
+        expect(init_board.check_for_winner).to be(nil)
+      end
+    end
+  end
 end
