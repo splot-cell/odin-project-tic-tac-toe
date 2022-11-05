1f5d20d863fee4b58bd6303287d54db66a02481f
Olly Seber <olly_s@hotmail.co.uk>
2022-11-05 08:57:17 -0700

    Add test for winning condition

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -3,12 +3,22 @@
 require_relative "../lib/tic_tac_toe"
 
 describe Board do
-  subject(:init_board) { described_class.new }
+  subject(:board_init) { described_class.new }
 
   describe "#check_for_winner" do
-    context "When the Board is initialized" do
-      it "Returns nil" do
-        expect(init_board.check_for_winner).to be(nil)
+    context "when the Board is initialized" do
+      it "returns nil" do
+        expect(board_init.check_for_winner).to be(nil)
+      end
+    end
+
+    context "when 'X' has won" do
+      subject(:board_x_win) { described_class.new }
+
+      it "returns 'X'" do
+        winning_positions = ['X', 'X', 'X', ' ', ' ', ' ', ' ', ' ', ' ']
+        board_x_win.instance_variable_set(:@cells, winning_positions)
+        expect(board_x_win.check_for_winner).to be('X')
       end
     end
   end
